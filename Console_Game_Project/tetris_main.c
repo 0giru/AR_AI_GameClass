c3219a9d8c395a06465bda557649ed366e16978e
Yeon_gil Kim <60814646+0giru@users.noreply.github.com>
2021-05-11 01:33:15 +0900

    Update tetris_main.c

--- Console_Game_Project/tetris_main.c
+++ Console_Game_Project/tetris_main.c
@@ -1,10 +1,8 @@
 #include "tetris.h"
 
 int main() {
-	PlaySound(TEXT("C:\\Users\\dusrl\\source\\repos\\Project1\\Project1\\tetris.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+	PlaySound(TEXT("tetris.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 
-	CursorView();
-	SetConsoleSize();
 	InitGame();
 	PlayGame();
 
